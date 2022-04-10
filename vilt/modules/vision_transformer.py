b77311212716d43f5a224097db8a80e6a4a823e1
ali <ali.mhd.work@gmail.com>
2022-04-10 13:32:18 +0300

    add mpp

--- vilt/modules/vision_transformer.py
+++ vilt/modules/vision_transformer.py
@@ -616,7 +616,7 @@ class VisionTransformer(nn.Module):
     def visual_embed(self, _x, max_image_len=200, mask_it=False):
         _, _, ph, pw = self.patch_embed.getDims()
         x,label = self.patch_embed(_x)
-
+        x= self.patch_embed.proj(_x)
         ##
         x_mask = (_x.sum(dim=1) != 0).float()[:, None, :, :]
         print("x_mask shape ",x_mask.shape )
